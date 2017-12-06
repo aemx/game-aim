c646c78278714bc9d19eaf8581723c7c56922571
Spencer Gunning <swcgunning@gmail.com>
2017-12-05 19:02:09 -0500

    [2017r1] Print console list

---
+++ gameaim.py
@@ -0,0 +1,7 @@
+print(
+    "\nSelect consoles:\n\n" + \
+    "1.  Gameboy Advance   2.  Gamecube         3.  Neo Geo          4.  NES\n" + \
+    "5.  Nintendo Switch   6.  PC               7.  Playstation 1    8.  Playstation 2\n"  + \
+    "9.  Playstation 3     10. Playstation 4    11. Wii              12. Wii U\n" + \
+    "13. Xbox              14. Xbox 360\n"
+)
